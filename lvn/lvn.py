b0e7d123020324ecb9ca6ef383209a6bb6c5eab0
Lai-YT <381xvmvbib@gmail.com>
2024-07-11 11:56:49 +0800

    Refactor tuple access by named tuple

--- lvn/lvn.py
+++ lvn/lvn.py
@@ -6,9 +6,12 @@ __version__ = "0.1.0"
 
 import json
 import sys
+from collections import namedtuple
 from typing import Any, Dict, List, Tuple, Union
 
 import cfg
+
+Value = namedtuple("Value", ["op", "args"])
 
 
 def rename_args_between(instrs: List[cfg.Instr], old_name: str, new_name: str) -> None:
@@ -28,7 +31,7 @@ def lvn() -> None:
             row_num = 0
             # Map the tuple value to its canonical variable with the row number wrapped together.
             # A value consists of an operator, along with its row number (or constant value).
-            val2var: Dict[Tuple, Tuple[str, int]] = {}
+            val2var: Dict[Value, Tuple[str, int]] = {}
             # Map the variable to the row number of its value.
             var2num: Dict[str, int] = {}
 
@@ -39,7 +42,8 @@ def lvn() -> None:
                         continue
                     the_row_num = var2num[arg]
                     # NOTE: This requires the dict to be ordered, and the row number must exactly match the ordered we added.
-                    instr["args"][i] = list(val2var.values())[the_row_num][0]
+                    var, _ = list(val2var.values())[the_row_num]
+                    instr["args"][i] = var
 
             # Used to rename variables that are reassigned.
             lvn_number = 0
@@ -49,14 +53,12 @@ def lvn() -> None:
                     continue
 
                 if "dest" not in instr:
-                    # Not an assignment; only perform replacement.
-                    # NOTE: Always replace; may be the same.
-                    replace_args_with_canonical(instr)
+                    # Not an assignment.
                     continue
 
                 if instr["op"] == "const":
                     # NOTE: Since False is equal to 0 in Python, we have to include the type.
-                    val = "const", instr["value"], instr["type"]
+                    val = Value("const", (instr["value"], instr["type"]))
                 else:
                     # NOTE: If the variable isn't record, it's because such variable is defined in another basic block.
                     # In such case, there's no column number, we use the variable name directly in the tuple value.
@@ -67,23 +69,23 @@ def lvn() -> None:
                         op += instr["funcs"][0]
 
                     # Lookup the row numbers of the operands.
-                    row_nums: List[Union[str, int]] = []
-                    for arg in instr["args"]:
-                        row_nums.append(var2num.get(arg, arg))
+                    row_nums: Tuple[Union[str, int], ...] = tuple(
+                        var2num.get(arg, arg) for arg in instr["args"]
+                    )
                     if op == "add":
                         # Commutativity; sort the args to canonicalize.
                         # NOTE: Row numbers can be str if is defined in other basic block.
-                        row_nums.sort(key=lambda x: str(x))
-                    val = op, *row_nums
+                        val = Value(*sorted(row_nums, key=lambda x: str(x)))
+                    val = Value(op, row_nums)
 
                 if val in val2var:
                     # The value has been computed before;
                     # map it to the canonical variable without adding a new row.
                     var, the_row_num = val2var[val]
-                    # Replace the instruction as directly using the canonical variable to eliminate the computation.
-                    # TODO: constant propagation
-                    instr["op"] = "id"
-                    instr["args"] = [var]
+                    if val.op != "const":
+                        # Replace the instruction as directly using the canonical variable to eliminate the computation.
+                        instr["op"] = "id"
+                        instr["args"] = [var]
                 else:
                     # If the variable is going to be reassigned, we give it an unique name so that the value is correct when later instructions use it as the canonical variable.
                     dest = instr["dest"]
@@ -104,8 +106,6 @@ def lvn() -> None:
                     row_num += 1
                     val2var[val] = dest, the_row_num
 
-                    # As long as all previous instructions are canonicalize,
-                    # there's no difference between replacing here or earlier.
                     replace_args_with_canonical(instr)
 
                 var2num[instr["dest"]] = the_row_num
