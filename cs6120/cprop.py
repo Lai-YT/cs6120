26ffb8dec335092057738f2078067c52f4b5ba14
Lai-YT <381xvmvbib@gmail.com>
2024-07-20 16:53:18 +0800

    Fix incorrect merge function of constant propagation (#11)
    The output sets of the predecessors should not be merged using a simple
    intersection. A variable can be missing from some of the predecessors as
    long as it is always present with the same known constant value.
    To differentiate between having known values and being missing,
    variables with unknown values are now included in the set with a special
    marker value ("?").

--- cs6120/cprop.py
+++ cs6120/cprop.py
@@ -2,11 +2,11 @@
 
 - At the entry and exit of each basic block, the analysis determines which variable contains a constant value.
 - This is a forward analysis.
-- For blocks with multiple predecessors, the sets are merged using `Intersection`.
+- For blocks with multiple predecessors, the sets are merged using a special `Intersection`.
 """
 
 import operator
-from typing import Any, Callable, Dict, List
+from typing import Any, Callable, Dict, Final, Iterable, List
 
 from type import Block
 
@@ -35,6 +35,27 @@ EVAL_OPS: Dict[str, Callable] = {
     "or": operator.or_,
 }
 
+UNKNOWN: Final = "?"
+
+
+def is_const(v: Any) -> bool:
+    return v != UNKNOWN
+
+
+def merge(dicts: Iterable[Dict[str, Any]]) -> Dict[str, Any]:
+    """
+    A variable can be missing from some of the predecessors as long as it is always present with the same known constant value.
+    """
+    res = {}
+    for d in dicts:
+        for k, v in d.items():
+            if k not in res:
+                res[k] = v
+            elif v != res[k]:
+                # The value diverges; mark as unknown.
+                res[k] = UNKNOWN
+    return res
+
 
 def out(b: Block, in_: Dict[str, Any]) -> Dict[str, Any]:
     """Computes the OUT set (a dict is a kind of set) for a block, performing constant propagation.
@@ -44,30 +65,28 @@ def out(b: Block, in_: Dict[str, Any]) -> Dict[str, Any]:
         in_: The IN set of constant value mappings for the block.
 
     Returns:
-        The OUT set that maps names to their constant values.
+        The OUT set that maps names to their constant values, with unknowns mapping to "?".
     """
+    # NOTE: Function arguments are not included in the IN set; they are considered as unknowns.
     res = in_.copy()  # not to modify the input
     for instr in b:
-        if "op" not in instr:
+        if "op" not in instr or "dest" not in instr:
             continue
+
         op: str = instr["op"]
+        dest: str = instr["dest"]
         if op == "const":
-            res[instr["dest"]] = instr["value"]
-        elif op == "id":
-            arg: str = instr["args"][0]
-            if arg in res:
-                # Propagate the constant.
-                res[instr["dest"]] = res[arg]
-            else:
-                # Assigned with a non-constant variable; no longer a constant.
-                res.pop(instr["dest"], None)
-        elif op in EVAL_OPS:
+            res[dest] = instr["value"]
+        elif op == "id" and instr["args"][0] in res and is_const(res[instr["args"][0]]):
+            # Propagate the constant.
+            res[dest] = res[instr["args"][0]]
+        elif op in EVAL_OPS and all(
+            arg in res and is_const(res[arg]) for arg in instr["args"]
+        ):
             # If all of the arguments are constants, calculate them.
-            args: List[str] = instr["args"]
-            if all(arg in res for arg in args):
-                vals: List[Any] = [res[arg] for arg in args]
-                res[instr["dest"]] = EVAL_OPS[op](*vals)
-            else:
-                # Assigned with a non-constant variable; no longer a constant.
-                res.pop(instr["dest"], None)
+            vals = [res[arg] for arg in instr["args"]]
+            res[dest] = EVAL_OPS[op](*vals)
+        else:
+            # Assigned with a non-constant variable; no longer a constant.
+            res[dest] = UNKNOWN
     return res
