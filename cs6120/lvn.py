70acfbb7dead90685e0adf4958be27aeed90a8e6
Lai-YT <381xvmvbib@gmail.com>
2024-07-26 11:24:43 +0800

    Extract functions & fix incorrect value assignment
    The value was reassigned unconditionally after checking for commutativity,
    potentially overwriting the result of sorting.
    Additionally, the canonicalization can be performed before extracting the
    value representation, making the logic more straightforward.

--- cs6120/lvn.py
+++ cs6120/lvn.py
@@ -31,6 +31,64 @@ def is_commutative(op: str) -> bool:
     return op in ("add", "mul", "eq", "add", "or")
 
 
+def extract_value_repr(instr: Instr, var2num: Dict[str, int]) -> Value:
+    """
+    Returns:
+        A Value namedtuple containing the operation and its arguments.
+        For constant operations, the tuple includes the constant value and type.
+        For other operations, the arguments are replaced with their corresponding
+        row numbers if available, and sorted if the operation is commutative.
+    """
+    if instr["op"] == "const":
+        # NOTE: Since False is equal to 0 in Python, we have to include the type.
+        return Value("const", (instr["value"], instr["type"]))
+    # NOTE: If the variable isn't record, it's because such variable is defined in another basic block.
+    # In such case, there's no column number, we use the variable name directly in the tuple value.
+
+    # For function calls, we postfix the function name to the op to differentiate.
+    op = instr["op"]
+    if "funcs" in instr:
+        op += instr["funcs"][0]
+
+    # Lookup the row numbers of the operands.
+    row_nums: Tuple[Union[str, int], ...] = tuple(
+        var2num.get(arg, arg) for arg in instr["args"]
+    )
+    if is_commutative(op):
+        # Commutativity; sort the args to canonicalize.
+        # NOTE: Row numbers can be str if is defined in other basic block.
+        return Value(*sorted(row_nums, key=lambda x: str(x)))
+    return Value(op, row_nums)
+
+
+def rename_if_will_be_reassigned(
+    instr: Instr, later_instrs: List[Instr], next_lvn_number: int
+) -> str:
+    """Renames a variable if it will be reassigned in subsequent instructions.
+
+    Checks if the destination variable of the given instruction will be
+    reassigned in any of the later instructions. If so, generates a new
+    unique name for the variable and updates all references to the variable
+    in the instructions up to and including the reassignment.
+
+    Args:
+        instr: The current instruction whose destination variable may be reassigned.
+        later_instrs: A list of subsequent instructions to check for reassignment.
+        next_lvn_number: The next available unique number for renaming variables.
+
+    Returns:
+        The new name of the variable if it was reassigned, otherwise the original name.
+    """
+    for i, later_instr in enumerate(later_instrs):  # peek the later instructions
+        if instr["dest"] == later_instr.get("dest", None):
+            new_name = f"{instr['dest']}.{next_lvn_number}"
+            # all args between should be updated to use the new name.
+            # i + 1 because may be used in the reassignment.
+            rename_args_between(later_instrs[: i + 1], instr["dest"], new_name)
+            return new_name
+    return instr["dest"]
+
+
 def lvn() -> None:
     prog: Dict[str, List[Dict[str, Any]]] = json.load(sys.stdin)
 
@@ -66,27 +124,8 @@ def lvn() -> None:
                     replace_args_with_canonical(instr)
                     continue
 
-                if instr["op"] == "const":
-                    # NOTE: Since False is equal to 0 in Python, we have to include the type.
-                    val = Value("const", (instr["value"], instr["type"]))
-                else:
-                    # NOTE: If the variable isn't record, it's because such variable is defined in another basic block.
-                    # In such case, there's no column number, we use the variable name directly in the tuple value.
-
-                    # For function calls, we postfix the function name to the op to differentiate.
-                    op = instr["op"]
-                    if "funcs" in instr:
-                        op += instr["funcs"][0]
-
-                    # Lookup the row numbers of the operands.
-                    row_nums: Tuple[Union[str, int], ...] = tuple(
-                        var2num.get(arg, arg) for arg in instr["args"]
-                    )
-                    if is_commutative(op):
-                        # Commutativity; sort the args to canonicalize.
-                        # NOTE: Row numbers can be str if is defined in other basic block.
-                        val = Value(*sorted(row_nums, key=lambda x: str(x)))
-                    val = Value(op, row_nums)
+                replace_args_with_canonical(instr)
+                val: Value = extract_value_repr(instr, var2num)
 
                 if val in val2var and not has_side_effect(val.op):
                     # The value has been computed before;
@@ -98,25 +137,17 @@ def lvn() -> None:
                         instr["args"] = [var]
                 else:
                     # If the variable is going to be reassigned, we give it an unique name so that the value is correct when later instructions use it as the canonical variable.
-                    dest = instr["dest"]
-                    for j in range(i + 1, len(block)):  # peek the later instructions
-                        later_instr = block[j]
-                        if dest == later_instr.get("dest", None):
-                            new_name = f"{dest}.{lvn_number}"
-                            lvn_number += 1
-                            # all args between should be updated to use the new name.
-                            # j + 1 because may be used in the reassignment.
-                            rename_args_between(block[i + 1 : j + 1], dest, new_name)
-                            dest = new_name
-                            instr["dest"] = dest
-                            break
+                    dest = rename_if_will_be_reassigned(
+                        instr, block[i + 1 :], lvn_number
+                    )
+                    if dest != instr["dest"]:
+                        instr["dest"] = dest
+                        lvn_number += 1
 
                     # A new value.
                     the_row_num = row_num
                     row_num += 1
                     val2var[val] = dest, the_row_num
-
-                    replace_args_with_canonical(instr)
 
                 var2num[instr["dest"]] = the_row_num
 
