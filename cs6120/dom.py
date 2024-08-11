56da63dfbb8743c89212aa5b59d683afbe3dbdec
Lai-YT <381xvmvbib@gmail.com>
2024-08-11 16:57:25 +0800

    Add dominance analyses (#14)
    * Wrap control flow graph as an object
    
    * Add tests for dominators
    
    * Find dominators for a function
    
    * Construct the dominance tree
    
    * Compute the dominance frontier
    
    * Fix missing child when idom is not predecessor
    
    The intermediate dominator (idom) may be the grand-predecessor when
    there exists the dimand-shaped control flow graph.

---
+++ cs6120/dom.py
@@ -0,0 +1,112 @@
+#!/usr/bin/env python3
+
+import argparse
+import functools
+import json
+import operator
+import sys
+from typing import Any, Dict, List, Optional, Set
+
+from cfg import ControlFlowGraph
+
+
+def get_dom(cfg: ControlFlowGraph) -> Dict[str, Set[str]]:
+    # The operator is set intersection; initialized with the set of all blocks.
+    dom: Dict[str, Set[str]] = {b: set(cfg.blocks) for b in cfg.blocks}
+    dom[cfg.entry] = {cfg.entry}
+    changed = True
+    while changed:
+        changed = False
+        for vertex in cfg.blocks:
+            new_dom = set([vertex])
+            try:
+                new_dom |= functools.reduce(
+                    operator.and_, (dom[x] for x in cfg.predecessors_of(vertex))
+                )
+            except TypeError:
+                # The vertex has no predecessor.
+                # It's the entry block, or it's unreachable.
+                pass
+            if new_dom != dom[vertex]:
+                dom[vertex] = new_dom
+                changed = True
+    return dom
+
+
+def dom_tree(cfg: ControlFlowGraph) -> Dict[str, List[str]]:
+    dom = get_dom(cfg)
+    tree: Dict[str, List[str]] = {b: [] for b in cfg.blocks}
+    for b in cfg.blocks:
+        idom = intermediate_dominator_of(dom, b)
+        if idom is not None:
+            tree[idom].append(b)
+    return {b: sorted(l) for b, l in tree.items()}
+
+
+def intermediate_dominator_of(dom: Dict[str, Set[str]], y: str) -> Optional[str]:
+    """
+    Returns:
+        The intermediate dominator of `y`; None if `y` is the entry block, which has no intermediate dominator.
+    """
+    # For the block x to be the intermediate dominator of y, all other dominators of y should also dominates x.
+    strict_dom_y = dom[y] - set([y])
+    for x in strict_dom_y:
+        if all(xx in dom[x] for xx in strict_dom_y):
+            return x
+    return None
+
+
+def dom_front(cfg: ControlFlowGraph) -> Dict[str, List[str]]:
+    # If A dominates B but not C, where C is a successor of B, then C is in the dominance frontiers of A.
+    dom = get_dom(cfg)
+    front: Dict[str, List[str]] = {b: [] for b in cfg.blocks}
+    for c in cfg.blocks:
+        for b in cfg.predecessors_of(c):
+            for a in dom[b]:
+                # If a == c, there's a loop, and c is the header.
+                # It's a frontier of itself.
+                if a not in dom[c] or a == c:
+                    front[a].append(c)
+    return {b: sorted(l) for b, l in front.items()}
+
+
+def set2sortedlist(o) -> List:
+    if isinstance(o, set):
+        return sorted(list(o))
+    raise TypeError(f"Object of type {o.__class__.__name__} is not JSON serializable")
+
+
+COMMANDS = {
+    "dom": get_dom,
+    "tree": dom_tree,
+    "front": dom_front,
+}
+
+
+def main() -> None:
+    parser = argparse.ArgumentParser(sys.argv[0])
+    parser.add_argument(
+        "cmd",
+        choices=COMMANDS.keys(),
+        metavar="CMD",
+        help=f"[{'|'.join(COMMANDS.keys())}]",
+    )
+    args = parser.parse_args()
+
+    prog: Dict[str, List[Dict[str, Any]]] = json.load(sys.stdin)
+
+    for func in prog["functions"]:
+        cfg = ControlFlowGraph(func["instrs"])
+        cfg.ensure_entry()
+        print(
+            json.dumps(
+                COMMANDS[args.cmd](cfg),
+                default=set2sortedlist,
+                indent=2,
+                sort_keys=True,
+            )
+        )
+
+
+if __name__ == "__main__":
+    main()
