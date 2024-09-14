c37f1f46985c2bf0c17677e0c57c67cbc6e25dde
Lai-YT <381xvmvbib@gmail.com>
2024-09-14 21:01:08 +0800

    Implement trivial constant folding pass
    The trivial constant folding pass folds binary operations on integers.
    It is able to fold identity functions, as well as multiplications by 0
    and divisions by 1. Notably, I didn't handle cases where both operands
    are constants, as LLVM automatically folds them even with `O0`.

--- cs6120/llvm-pass/pass/TrivialConstantFolding.cpp
+++ cs6120/llvm-pass/pass/TrivialConstantFolding.cpp
@@ -1,3 +1,10 @@
+#include "llvm/IR/Constants.h"
+#include "llvm/IR/InstrTypes.h"
+#include "llvm/IR/Instruction.h"
+#include "llvm/IR/Instructions.h"
+#include "llvm/IR/Module.h"
+#include "llvm/IR/PassManager.h"
+#include "llvm/IR/Value.h"
 #include "llvm/Pass.h"
 #include "llvm/Passes/PassBuilder.h"
 #include "llvm/Passes/PassPlugin.h"
@@ -10,10 +17,75 @@ namespace {
 struct TrivialConstantFoldingPass
     : public PassInfoMixin<TrivialConstantFoldingPass> {
   PreservedAnalyses run(Module &M, ModuleAnalysisManager &AM) {
+    bool Changed = false;
     for (auto &F : M) {
-      errs() << "I saw a function called " << F.getName() << "!\n";
+      for (auto &BB : F) {
+        for (auto &I : BB) {
+          if (auto *BOP = dyn_cast<BinaryOperator>(&I)) {
+            auto *LHSC = dyn_cast<ConstantInt>(BOP->getOperand(0));
+            auto *RHSC = dyn_cast<ConstantInt>(BOP->getOperand(1));
+
+            if (LHSC && RHSC) {
+              errs() << "Warning: It's unusual that Clang not constant fold "
+                        "this.\n";
+              continue;
+            }
+
+            Value *Result = nullptr;
+            if (LHSC) {
+              if (LHSC->isZero()) {
+                switch (BOP->getOpcode()) {
+                case Instruction::Add:
+                  Result = BOP->getOperand(1);
+                  break;
+                case Instruction::Mul:
+                  Result = LHSC; // 0
+                  break;
+                default:
+                  break;
+                }
+              } else if (LHSC->isOne()) {
+                switch (BOP->getOpcode()) {
+                case Instruction::Mul:
+                  Result = RHSC;
+                  break;
+                default:
+                  break;
+                }
+              }
+            } else if (RHSC) {
+              if (RHSC->isZero()) {
+                switch (BOP->getOpcode()) {
+                case Instruction::Add:
+                case Instruction::Sub:
+                  Result = LHSC;
+                  break;
+                case Instruction::Mul:
+                  Result = RHSC; // 0
+                  break;
+                default:
+                  break;
+                }
+              } else if (RHSC->isOne()) {
+                switch (BOP->getOpcode()) {
+                case Instruction::Mul:
+                case Instruction::SDiv:
+                  Result = LHSC;
+                  break;
+                default:
+                  break;
+                }
+              }
+            }
+            if (Result) {
+              BOP->replaceAllUsesWith(Result);
+              Changed = true;
+            }
+          }
+        }
+      }
     }
-    return PreservedAnalyses::all();
+    return Changed ? PreservedAnalyses::none() : PreservedAnalyses::all();
   }
 };
 
---
+++ cs6120/llvm-pass/test/TrivialConstantFolding/v0_sub.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main() {
+  int a = 10;
+  int b = a - 0;
+  printf("%d\n", b);
+  return 0;
+}
